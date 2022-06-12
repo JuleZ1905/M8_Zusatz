f44f5830714d616761eac4c410abdbe619cf0687
JuleZ_2k4 <8096@htl.rennweg.at>
2022-06-12 22:42:10 +0200

    small changes

--- lib/fortune_db.sql
+++ lib/fortune_db.sql
@@ -3,6 +3,7 @@ DROP DATABASE IF EXISTS fortune_db;
 CREATE DATABASE fortune_db;
 
 USE fortune_db;
+
 DROP TABLE IF EXISTS fortune_messege;
 
 CREATE TABLE fortune_message (
@@ -10,85 +11,165 @@ CREATE TABLE fortune_message (
     message VARCHAR(255) NOT NULL
 );
 
-INSERT INTO fortune_message (message) VALUES
-("Today it's up to you to create the peacefulness you long for."),
-("A friend asks only for your time not your money."),
-("If you refuse to accept anything but the best, you very often get it."),
-("A smile is your passport into the realm of possibility."),
-("Your high-minded principles spell success."),
-("Change can hurt, but it leads a path to something better."),
-("Today is the most important day in your life."),
-("You can always find happiness at work on Friday after you finish Saturday."),
-("Your life does not get better by chance. It gets better by change."),
-("You can't love life until you live the life you love."),
-("Your hard work will soon make you happy."),
-("You can't go wrong with a smile."),
-("You'll make life a lot easier if you dress like a pirate."),
-("Enjoy the good luck a companion brings you."),
-("People are naturally attracted to you."),
-("Hidden in a valley beside an open stream- This will be the type of place where you will find your dream."),
-("A chance meeting opens new doors to success and friendship."),
-("You learn from your mistakes... You will learn a lot today."),
-("If you have something good in your life, don't let it go!"),
-("What ever you're goal is in life, embrace it visualize it, and for it will be yours."),
-("Your shoes will make you happy today."),
-("You cannot love life until you live the life you love."),
-("Be on the lookout for coming events; They cast their shadows beforehand."),
-("Land is always on the mind of a flying bird."),
-("The man or woman you desire feels the same about you."),
-("A dream you have will come true."),
-("Our deeds determine us, as much as we determine our deeds."),
-("The fortune you seek is in another cookie."),
-("A closed mouth gathers no feet."),
-("A conclusion is simply the place where you got tired of thinking."),
-("A cynic is only a frustrated optimist."),
-("A foolish man listens to his heart. A wise man listens to cookies."),
-("You will die alone and poorly dressed."),
-("A fanatic is one who can't change his mind, and won't change the subject."),
-("If you look back, you'll soon be going that way."),
-("You will live long enough to open many fortune cookies."),
-("An alien of some sort will be appearing to you shortly."),
-("Do not mistake temptation for opportunity."),
-("Flattery will go far tonight."),
-("He who laughs at himself never runs out of things to laugh at."),
-("He who laughs last is laughing at you."),
-("He who throws dirt is losing ground."),
-("Some men dream of fortunes, others dream of cookies."),
-("The greatest danger could be your stupidity."),
-("We don't know the future, but here's a cookie."),
-("The world may be your oyster, but it doesn't mean you'll get its pearl."),
-("You will be hungry again in one hour."),
-("The road to riches is paved with homework."),
-("You can always find happiness at work on Friday."),
-("Actions speak louder than fortune cookies."),
-("Because of your melodic nature, the moonlight never misses an appointment."),
-("Don't behave with cold manners."),
-("Don't forget you are always on our minds."),
-("Fortune not found? Abort, Retry, Ignore."),
-("Help! I am being held prisoner in a fortune cookie factory."),
-("It's about time I got out of that cookie."),
-("Never forget a friend. Especially if he owes you."),
-("Never wear your best pants when you go to fight for freedom."),
-("Only listen to the fortune cookie; disregard all other fortune telling units."),
-("It is a good day to have a good day."),
-("All fortunes are wrong except this one."),
-("Someone will invite you to a Karaoke party."),
-("That wasn't chicken."),
-("There is no mistake so great as that of being always right."),
-("You love Chinese food."),
-("I am worth a fortune."),
-("No snowflake feels responsible in an avalanche."),
-("You will receive a fortune cookie."),
-("Some fortune cookies contain no fortune."),
-("Don't let statistics do a number on you."),
-("You are not illiterate."),
-("May you someday be carbon neutral."),
-("You have rice in your teeth."),
-("Avoid taking unnecessary gambles. Lucky numbers: 12, 15, 23, 28, 37"),
-("Ask your mom instead of a cookie."),
-("This cookie contains 117 calories."),
-("Hard work pays off in the future. Laziness pays off now."),
-("You think it's a secret, but they know."),
-("If a turtle doesn't have a shell, is it naked or homeless?"),
-("Change is inevitable, except for vending machines."),
-("Don't eat the paper.");
+
+INSERT INTO fortune_message (message)
+VALUES
+    (
+        "Today it's up to you to create the peacefulness you long for."
+    ),
+    (
+        "A friend asks only for your time not your money."
+    ),
+    (
+        "If you refuse to accept anything but the best, you very often get it."
+    ),
+    (
+        "A smile is your passport into the realm of possibility."
+    ),
+    ("Your high-minded principles spell success."),
+    (
+        "Change can hurt, but it leads a path to something better."
+    ),
+    ("Today is the most important day in your life."),
+    (
+        "You can always find happiness at work on Friday after you finish Saturday."
+    ),
+    (
+        "Your life does not get better by chance. It gets better by change."
+    ),
+    (
+        "You can't love life until you live the life you love."
+    ),
+    ("Your hard work will soon make you happy."),
+    ("You can't go wrong with a smile."),
+    (
+        "You'll make life a lot easier if you dress like a pirate."
+    ),
+    ("Enjoy the good luck a companion brings you."),
+    ("People are naturally attracted to you."),
+    (
+        "Hidden in a valley beside an open stream- This will be the type of place where you will find your dream."
+    ),
+    (
+        "A chance meeting opens new doors to success and friendship."
+    ),
+    (
+        "You learn from your mistakes... You will learn a lot today."
+    ),
+    (
+        "If you have something good in your life, don't let it go!"
+    ),
+    (
+        "What ever you're goal is in life, embrace it visualize it, and for it will be yours."
+    ),
+    ("Your shoes will make you happy today."),
+    (
+        "You cannot love life until you live the life you love."
+    ),
+    (
+        "Be on the lookout for coming events; They cast their shadows beforehand."
+    ),
+    ("Land is always on the mind of a flying bird."),
+    (
+        "The man or woman you desire feels the same about you."
+    ),
+    ("A dream you have will come true."),
+    (
+        "Our deeds determine us, as much as we determine our deeds."
+    ),
+    ("The fortune you seek is in another cookie."),
+    ("A closed mouth gathers no feet."),
+    (
+        "A conclusion is simply the place where you got tired of thinking."
+    ),
+    ("A cynic is only a frustrated optimist."),
+    (
+        "A foolish man listens to his heart. A wise man listens to cookies."
+    ),
+    ("You will die alone and poorly dressed."),
+    (
+        "A fanatic is one who can't change his mind, and won't change the subject."
+    ),
+    (
+        "If you look back, you'll soon be going that way."
+    ),
+    (
+        "You will live long enough to open many fortune cookies."
+    ),
+    (
+        "An alien of some sort will be appearing to you shortly."
+    ),
+    ("Do not mistake temptation for opportunity."),
+    ("Flattery will go far tonight."),
+    (
+        "He who laughs at himself never runs out of things to laugh at."
+    ),
+    ("He who laughs last is laughing at you."),
+    ("He who throws dirt is losing ground."),
+    (
+        "Some men dream of fortunes, others dream of cookies."
+    ),
+    ("The greatest danger could be your stupidity."),
+    ("We don't know the future, but here's a cookie."),
+    (
+        "The world may be your oyster, but it doesn't mean you'll get its pearl."
+    ),
+    ("You will be hungry again in one hour."),
+    ("The road to riches is paved with homework."),
+    (
+        "You can always find happiness at work on Friday."
+    ),
+    ("Actions speak louder than fortune cookies."),
+    (
+        "Because of your melodic nature, the moonlight never misses an appointment."
+    ),
+    ("Don't behave with cold manners."),
+    ("Don't forget you are always on our minds."),
+    ("Fortune not found? Abort, Retry, Ignore."),
+    (
+        "Help! I am being held prisoner in a fortune cookie factory."
+    ),
+    ("It's about time I got out of that cookie."),
+    (
+        "Never forget a friend. Especially if he owes you."
+    ),
+    (
+        "Never wear your best pants when you go to fight for freedom."
+    ),
+    (
+        "Only listen to the fortune cookie; disregard all other fortune telling units."
+    ),
+    ("It is a good day to have a good day."),
+    ("All fortunes are wrong except this one."),
+    ("Someone will invite you to a Karaoke party."),
+    ("That wasn't chicken."),
+    (
+        "There is no mistake so great as that of being always right."
+    ),
+    ("You love Chinese food."),
+    ("I am worth a fortune."),
+    (
+        "No snowflake feels responsible in an avalanche."
+    ),
+    ("You will receive a fortune cookie."),
+    ("Some fortune cookies contain no fortune."),
+    ("Don't let statistics do a number on you."),
+    ("You are not illiterate."),
+    ("May you someday be carbon neutral."),
+    ("You have rice in your teeth."),
+    (
+        "Avoid taking unnecessary gambles. Lucky numbers: 12, 15, 23, 28, 37"
+    ),
+    ("Ask your mom instead of a cookie."),
+    ("This cookie contains 117 calories."),
+    (
+        "Hard work pays off in the future. Laziness pays off now."
+    ),
+    ("You think it's a secret, but they know."),
+    (
+        "If a turtle doesn't have a shell, is it naked or homeless?"
+    ),
+    (
+        "Change is inevitable, except for vending machines."
+    ),
+    ("Don't eat the paper.");
